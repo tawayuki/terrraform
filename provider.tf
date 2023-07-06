2ad4351c5b3685d84e9671e12caf914f8ea5f7ce
tawayuki <yuki-tawa@gmogshd.com>
2023-07-06 08:35:09 +0000

    first commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "ap-northeast-1"
+  access_key = "AKIASAZHOIUY6THBO54S"
+  secret_key = "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"
   default_tags {
     tags = {
       Name = "tawa"
