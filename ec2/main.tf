5d471fcc667eecfc4b2006f4c832678b840330e0
tawayuki <138635473+tawayuki@users.noreply.github.com>
2023-07-06 16:23:18 +0900

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,1 +1,7 @@
-
+resource "aws_instance" "server" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id       = var.public_subnet_id
+  security_groups = [var.sg_id]
+#  iam_instance_profile = var.iam_instance_profile
+}
