f43fbd53598c8ff75b1ea695753b8c956cf118f1
tawayuki <yuki-tawa@gmogshd.com>
2023-07-07 04:28:45 +0000

    role

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "server" {
   instance_type          = var.instance_type
   subnet_id       = var.public_subnet_id
   security_groups = [var.sg_id]
-#  iam_instance_profile = var.iam_instance_profile
+  iam_instance_profile = var.iam_instance_profile
 }
 
