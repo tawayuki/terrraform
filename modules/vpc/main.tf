ad4231be9a8730ac93f8966f0a93400ea7f76087
tawayuki <yuki-tawa@gmogshd.com>
2023-07-07 01:59:16 +0000

    publicIP

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,6 +7,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.cidr_public1
   availability_zone       = var.az1
+  map_public_ip_on_launch = true
   tags = {
    Name = "public"
  }
