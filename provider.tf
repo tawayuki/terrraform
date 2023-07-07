2664dcc1b5a14924d8b52e9642fb330de328ff2d
tawayuki <yuki-tawa@gmogshd.com>
2023-07-07 02:06:00 +0000

    safe

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "ap-northeast-1"
-  access_key = "AKIASAZHOIUY6THBO54S"
-  secret_key = "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"
   default_tags {
     tags = {
       Name = "tawa"
