bda74cb598dce7116c0b1a2906d5b8bdd47d07df
tawayuki <yuki-tawa@gmogshd.com>
2023-07-07 04:47:40 +0000

    add

--- main.tf
+++ main.tf
@@ -8,29 +8,8 @@ module "network" {
   az1 = "ap-northeast-1a"
   az2 = "ap-northeast-1c"
 }
-
-module "ec2" {
-  source   = "./modules/ec2"
-  ami                    = "ami-0cfc97bf81f2eadc4"
-  instance_type          = "t2.micro"
-  public_subnet_id       = module.network.public_subnet_id
-  sg_id            = module.ec2-sg.sg_id
-  iam_instance_profile   = module.iam_role.iam_role_name
-}
-
-module ec2-sg {
-  source = "./modules/securitygroup"
-  sg_config = {
-    name        = "ec2-sg"
-    vpc_id      = module.network.vpc_id
-    protocol    = "tcp"
-    port        = [22, 80, 443]
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-}
 module "iam_role" {
   source = "./modules/iamrole"
-  instance_profile_name = "my-instance-role"
   iam_role_name             = "tawa-role"
   iam_role_assume_role_policy = {
     Version = "2012-10-17"
@@ -45,6 +24,25 @@ module "iam_role" {
     ]
   }
   iam_role_policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+module "ec2" {
+  source   = "./modules/ec2"
+  ami                    = "ami-0cfc97bf81f2eadc4"
+  instance_type          = "t2.micro"
+  public_subnet_id       = module.network.public_subnet_id
+  sg_id            = module.ec2-sg.sg_id
+  iam_instance_profile   = module.iam_role.instanceprofile
+}
+
+module ec2-sg {
+  source = "./modules/securitygroup"
+  sg_config = {
+    name        = "ec2-sg"
+    vpc_id      = module.network.vpc_id
+    protocol    = "tcp"
+    port        = [22, 80, 443]
+    cidr_blocks = ["10.0.0.0/16"]
+  }
 }
 
 
