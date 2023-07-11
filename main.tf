1c00eb59651f0310f825d886ac6fe51d57477757
tawayuki <yuki-tawa@gmogshd.com>
2023-07-11 06:47:58 +0000

    alb_add

--- main.tf
+++ main.tf
@@ -8,6 +8,26 @@ module "network" {
   az1 = "ap-northeast-1a"
   az2 = "ap-northeast-1c"
 }
+
+module "ec2" {
+  source   = "./modules/ec2"
+  ami                    = "ami-0cfc97bf81f2eadc4"
+  instance_type          = "t2.micro"
+  public_subnet_id       = module.network.public_subnet_id
+  sg_id            = module.ec2-sg.sg_id
+  iam_instance_profile   = module.iam_role.instanceprofile
+}
+
+module "ec2-sg" {
+  source = "./modules/securitygroup"
+  sg_config = {
+    name        = "ec2-sg"
+    vpc_id      = module.network.vpc_id
+    protocol    = "tcp"
+    port        = [22, 80, 443]
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
 module "iam_role" {
   source = "./modules/iamrole"
   iam_role_name             = "tawa-role"
@@ -25,25 +45,41 @@ module "iam_role" {
   }
   iam_role_policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
-module "ec2" {
-  source   = "./modules/ec2"
-  ami                    = "ami-0cfc97bf81f2eadc4"
-  instance_type          = "t2.micro"
-  public_subnet_id       = module.network.public_subnet_id
-  sg_id            = module.ec2-sg.sg_id
-  iam_instance_profile   = module.iam_role.instanceprofile
-}
 
-module ec2-sg {
-  source = "./modules/securitygroup"
-  sg_config = {
-    name        = "ec2-sg"
+module alb_sg {
+  source = "./modules/securitygroup_alb"
+  alb_sg_config = {
+    name        = "alb-sg"
     vpc_id      = module.network.vpc_id
     protocol    = "tcp"
-    port        = [22, 80, 443]
-    cidr_blocks = ["10.0.0.0/16"]
+    port        = [443]
+    cidr_blocks = ["153.122.166.254/32"]
   }
 }
-
+module lb {
+  source = "./modules/alb"
+  security_groups = module.alb_sg.alb_sg_id
+  target_id      =  [module.ec2.instance_id]
+  lb_base_config = {
+    public_subnet_id           = module.network.public_subnet_ids
+    sg_id                      = module.alb_sg.alb_sg_id
+    Name                    = "Tawa"
+    enable_deletion_protection = false
+  }
+  lb_target_config = {
+    port        = 80
+    protocol    = "HTTP"
+    vpc_id      = module.network.vpc_id
+    path        = "/index.html"
+    instance_id = module.ec2.instance_id
+    }
+    lb_listener_config = {
+    port        = 80
+    protocol    = "HTTP"
+    vpc_id      = module.network.vpc_id
+    path        = "/"
+    instance_id = module.ec2.instance_id
+  }
+}
 
 
